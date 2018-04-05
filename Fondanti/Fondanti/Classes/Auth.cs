81d6d1131014e7011cbb541de72fe21646558667
Jordy_Bosma <jordybosma@msn.com>
2018-04-05 13:54:26 +0200

    Connection code update 0.2

---
+++ Fondanti/Fondanti/Classes/Auth.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using WebMatrix.Data;
+
+/// <summary>
+/// Checks if the user input is equal to the SHA-Encrypted password.
+/// </summary>
+public class Auth  
+{
+    
+}
---
+++ Fondanti/Fondanti/Classes/SHAGenerate.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Security.Cryptography;
+using System.Text;
+using System.IO;
+/// <summary>
+/// SHA-Encrypts the password that was entered by the user.
+/// </summary>
+public class SHAGenerate
+{
+    public static string GenerateSHAString(string input)
+    {
+        SHA512 sha512 = SHA512Managed.Create();
+        byte[] bytes = Encoding.UTF8.GetBytes(input);
+        byte[] hash = sha512.ComputeHash(bytes);
+        return GetStringFromHash(hash);
+    }
+
+    private static string GetStringFromHash(byte[] hash)
+    {
+        StringBuilder result = new StringBuilder();
+        for (int i = 0; i < hash.Length; i++)
+        {
+            result.Append(hash[i].ToString("X2"));
+        }
+        return result.ToString();
+    }
+}
