b19a61fb931773922ae1354cd4436e352265605b
Jordy_Bosma <jordybosma@msn.com>
2018-04-05 19:06:33 +0200

    Password Hash & database conn reform

---
+++ Fondanti/Fondanti/App_Code/Auth.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using WebMatrix.Data;
+
+/// <summary>
+/// Checks if the user input is equal to the SHA-Encrypted password.
+/// </summary>
+public class Auth  
+{
+    private Database db;
+
+    /// <summary>
+    /// User login
+    /// </summary>
+    public int Login(string username, string password)
+    {
+        password = SHAGenerate.GenerateSHAString(password);
+        using (db = Database.Open("Db_Fondanti"))
+        {
+            int login = db.QueryValue("SELECT COUNT(*) FROM Accounts WHERE Username = @0 AND Password = @1", username, password);
+            return login;
+        }
+    }
+}
